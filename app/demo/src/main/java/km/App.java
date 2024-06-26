b7e1d09f634cf287d232b067dbb5fac7df43afb0
Bar1ss <kgubina206@gmail.com>
2024-06-26 17:01:56 +0500

    Up4

--- app/demo/src/main/java/km/App.java
+++ app/demo/src/main/java/km/App.java
@@ -16,7 +16,7 @@ public class App extends Application {
     private static Scene scene;
 
     @Override
-    public void start(Stage stage) throws IOException { 
+    public void start(Stage stage) throws IOException { //окно
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
         stage.show();
