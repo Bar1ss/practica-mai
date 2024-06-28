c36b76ff13657e61f3b3eeeb7be2cd8dc2029d9f
Bar1ss <kgubina206@gmail.com>
2024-06-28 11:35:24 +0500

    shishi

--- app/demo/src/main/java/km/App.java
+++ app/demo/src/main/java/km/App.java
@@ -1,10 +1,13 @@
 package km;
 
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.layout.FlowPane;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -25,6 +28,9 @@ public class App extends Application {
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
         stage.setTitle("Учёт расходов");
+        Image anotherIcon = new Image("file: HavnyShishi.png");
+        stage.getIcons().add(anotherIcon);
+
         stage.show();
     }
 
@@ -38,6 +44,19 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
+        String text = "Hello world!"; // строка для записи
+        try(FileOutputStream out=new FileOutputStream("notes.txt"); 
+                BufferedOutputStream bos = new BufferedOutputStream(out))
+        {
+            // перевод строки в байты
+            byte[] buffer = text.getBytes();
+            bos.write(buffer, 0, buffer.length);
+        }
+        catch(IOException ex){
+             
+            System.out.println(ex.getMessage());
+        } 
+
         launch();
     }
       
